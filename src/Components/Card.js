5e601d1b7c7403e6792f7a5d8ed5766c5b368d72
melobrunoti <melobrunoti@gmail.com>
2022-08-16 17:39:31 -0300

    out of stock logic

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -15,17 +15,28 @@ export default class Card extends Component {
         {products &&
           products.map(({ name, gallery, prices, inStock, category, id }) => {
             return (
-              <div key={name} className="product-card">
+              <div
+                key={name}
+                className={
+                  inStock ? 'product-card' : 'product-card out-of-stock'
+                }
+              >
                 <div className="product-card__image-container">
                   <img
                     src={gallery[0]}
                     alt={name}
                     className="product-card__image"
                   ></img>
-                  <Link to={`/product/${id}`} className="product-card__button">
-                    {' '}
-                    <img src={buyIcon} alt="go to cart" />{' '}
-                  </Link>
+                  {!inStock && <h3>Out of stock</h3>}
+                  {inStock && (
+                    <Link
+                      to={`/product/${id}`}
+                      className="product-card__button"
+                    >
+                      {' '}
+                      <img src={buyIcon} alt="go to cart" />{' '}
+                    </Link>
+                  )}
                 </div>
                 <div className="product-card__content">
                   <p>{name}</p>
